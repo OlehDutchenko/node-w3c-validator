5bb5828963e7f587aa051429913e5e5effa7d39d
dutchenkoOleg <dutchenko.o.wezom@gmail.com>
2017-07-04 14:56:07 +0300

    render-html

--- index.js
+++ index.js
@@ -10,36 +10,29 @@
 // ----------------------------------------
 
 const path = require('path');
-const VNU = require('./lib/vnu');
+const validate = require('./lib/validate');
 
 // ----------------------------------------
 // Private
 // ----------------------------------------
 
-const vnu = new VNU({
-	format: 'json',
-	skipNonHtml: true,
-	verbose: true
-});
 const testFilePath = path.join(process.cwd(), './tmp/index.html');
 
 // ----------------------------------------
 // Public
 // ----------------------------------------
 
-vnu.validate(testFilePath, {
-	format: 'json'
-}, function (err, stdout, stderr) {
+validate(testFilePath, {
+	format: 'text',
+	skipNonHtml: true,
+	verbose: true,
+	outputAsHtml: true
+}, function (err, output) {
 	if (err === null) {
 		return console.log('ok');
 	}
 
-	console.log(JSON.stringify(JSON.parse(stderr), null, '  '));
-	console.log(stderr);
-
-	// let output = VNU.transformOutput(stderr, 'text', 'html');
-
-	// console.log(output);
+	console.log(output);
 });
 
 // ----------------------------------------
