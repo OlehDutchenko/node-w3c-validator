b38d6ae0d0c398fa0687a4d8e903eee3f3672129
dutchenkoOleg <dutchenko.o.wezom@gmail.com>
2017-07-04 10:52:52 +0300

    initial

--- index.js
+++ index.js
@@ -9,13 +9,35 @@
 // Imports
 // ----------------------------------------
 
+const path = require('path');
+const VNU = require('./lib/vnu');
+
 // ----------------------------------------
 // Private
 // ----------------------------------------
 
+const vnu = new VNU({
+	format: 'json',
+	skipNonHtml: true,
+	verbose: true
+});
+const testFilePath = path.join(process.cwd(), './tmp/index.html');
+
 // ----------------------------------------
 // Public
 // ----------------------------------------
+
+vnu.validate(testFilePath, {
+	format: 'text'
+}, function (err, stdout, stderr) {
+	if (err === null) {
+		return console.log('ok');
+	}
+
+	console.log('error');
+	console.log(stdout);
+	console.log(stderr);
+});
 
 // ----------------------------------------
 // Exports
