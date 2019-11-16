5b4b203e10df2c606e1a6f458fd4a6214e474b79
Дутченко Олег <dutchenko.o@wezom.com.ua>
2019-11-16 09:47:00 +0200

    1.3.4 - Add prettier

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 [![npm](https://img.shields.io/badge/npm-install-orange.svg)](https://www.npmjs.com/package/node-w3c-validator)
 [![license](https://img.shields.io/badge/License-MIT-blue.svg)](https://github.com/dutchenkoOleg/node-w3c-validator/blob/master/LICENSE)
 [![Build Status](https://travis-ci.org/dutchenkoOleg/node-w3c-validator.svg?branch=master)](https://travis-ci.org/dutchenkoOleg/node-w3c-validator)
+[![code style: prettier](https://img.shields.io/badge/code_style-prettier-ff69b4.svg)](https://github.com/prettier/prettier)
 
 > _Wrapper for [The Nu Html Checker (v.Nu)](https://www.npmjs.com/package/vnu-jar)_
 
--- bin/cmd.js
+++ bin/cmd.js
@@ -19,19 +19,43 @@ program
 	.version(pkg.version)
 	.usage('[options] <pattern>')
 	.option('-i, --input [path]', 'Validate input path')
-	.option('-a, --asciiquotes', 'Specifies whether ASCII quotation marks are substituted for Unicode smart quotation marks in messages.')
-	.option('-e, --errors-only', 'Specifies that only error-level messages and non-document-error messages are reported (so that warnings and info messages are not reported)')
+	.option(
+		'-a, --asciiquotes',
+		'Specifies whether ASCII quotation marks are substituted for Unicode smart quotation marks in messages.'
+	)
+	.option(
+		'-e, --errors-only',
+		'Specifies that only error-level messages and non-document-error messages are reported (so that warnings and info messages are not reported)'
+	)
 	.option('-q, --exit-zero-always', 'Makes the checker exit zero even if errors are reported for any documents')
-	.option('--filterfile [filename]', 'Specifies a filename. Each line of the file contains either a regular expression or starts with "#" to indicate the line is a comment. Any error message or warning message that matches a regular expression in the file is filtered out (dropped/suppressed)')
-	.option('--filterpattern [pattern]', 'Specifies a regular-expression pattern. Any error message or warning message that matches the pattern is filtered out (dropped/suppressed)')
+	.option(
+		'--filterfile [filename]',
+		'Specifies a filename. Each line of the file contains either a regular expression or starts with "#" to indicate the line is a comment. Any error message or warning message that matches a regular expression in the file is filtered out (dropped/suppressed)'
+	)
+	.option(
+		'--filterpattern [pattern]',
+		'Specifies a regular-expression pattern. Any error message or warning message that matches the pattern is filtered out (dropped/suppressed)'
+	)
 	.option('-f, --format [gnu|xml|json|text|html]', 'Specifies the output format for reporting the results')
 	.option('-s, --skip-non-html', 'Skip documents that don’t have *.html, *.htm, *.xhtml, or *.xht extensions.')
 	.option('-H, --html', 'Forces any *.xhtml or *.xht documents to be parsed using the HTML parser')
-	.option('--no-langdetect', 'Disables language detection, so that documents are not checked for missing or mislabeled html[lang] attributes.')
-	.option('--no-stream', 'Forces all documents to be be parsed in buffered mode instead of streaming mode (causes some parse errors to be treated as non-fatal document errors instead of as fatal document errors)')
-	.option('-v, --verbose', 'Specifies "verbose" output (currently this just means that the names of files being checked are written to stdout)')
+	.option(
+		'--no-langdetect',
+		'Disables language detection, so that documents are not checked for missing or mislabeled html[lang] attributes.'
+	)
+	.option(
+		'--no-stream',
+		'Forces all documents to be be parsed in buffered mode instead of streaming mode (causes some parse errors to be treated as non-fatal document errors instead of as fatal document errors)'
+	)
+	.option(
+		'-v, --verbose',
+		'Specifies "verbose" output (currently this just means that the names of files being checked are written to stdout)'
+	)
 	.option('-o, --output [path]', 'Write reporting result to the path')
-	.option('-b, --buffersize <size>', '1024 * <size> Increase maxBuffer size for child_process.exec, if result output truncated')
+	.option(
+		'-b, --buffersize <size>',
+		'1024 * <size> Increase maxBuffer size for child_process.exec, if result output truncated'
+	)
 	.parse(process.argv);
 
 /**
@@ -65,7 +89,7 @@ function detectUserOptions () {
 		exec: {}
 	};
 
-	cliProps.forEach(prop => {
+	cliProps.forEach((prop) => {
 		let value = program[prop];
 
 		if ((prop === 'stream' || prop === 'langdetect') && value) {
@@ -95,7 +119,7 @@ function detectUserInput () {
 	if (typeof validatePath !== 'string') {
 		validatePath = process.cwd();
 	} else {
-		if (!(/^(http(s)?:)?\/\//i.test(validatePath))) {
+		if (!/^(http(s)?:)?\/\//i.test(validatePath)) {
 			if (/^\//.test(validatePath)) {
 				validatePath = path.resolve(validatePath);
 			}
